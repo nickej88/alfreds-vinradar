12686bf45e252d4f48a849e97ab1164456eae586
nickej88 <nickej88@gmail.com>
2026-05-27 19:26:24 +0200

    Update main.py

--- main.py
+++ main.py
@@ -354,7 +354,7 @@ def search_wines(search_term):
             )  
     
             message += (
-                f"🍷 {wine_name} {vintage}\n"
+                f"🍷 {producer}, {wine_name} | {vintage}\n"
                 f"🌍 {origin_level_1} - {origin_level_2}\n"
                 f"{price} kr\n"
                 f"🔗 {product_url}\n\n"
